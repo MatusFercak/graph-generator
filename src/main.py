451c9afbcbad19482a396e5342b1e5894e4737ea
Matúš Ferčák <74977647+MatusFercak@users.noreply.github.com>
2023-10-01 15:41:27 +0200

    patch

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ def main(context):
     plt.plot([1, 2, 3, 4])
     plt.ylabel('some numbers')
     buf = BytesIO()
+    plt.figure(figsize=(100, 100))
     plt.savefig(buf, format="png")
     # print(buf)
 
