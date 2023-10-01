c749767ce5a10ab049c7edd4a85c9e19f98d2668
Matúš Ferčák <74977647+MatusFercak@users.noreply.github.com>
2023-10-01 17:52:09 +0200

    patch

--- src/main.py
+++ src/main.py
@@ -3,22 +3,28 @@ from io import BytesIO
 
 
 def main(context):
-    plt.plot([1, 2, 3, 4])
-    plt.ylabel('some numbers')
-    buf = BytesIO()
-    plt.savefig(buf, format="png")
-    # print(buf)
+    if context.req.method == "GET":
+        context.log(context.req.body)
 
-    context.log(context)
+        x_axis = [1, 2, 3]
+        y_axis = ["A", "B", "C"]
+        plt.plot(x_axis, y_axis)
+
+        plt.xlabel('num')
+        plt.ylabel('let')
+
+        buf = BytesIO()
+        plt.savefig(buf, format="png")
+        return context.res.send(buf.getvalue())
 
     # If something goes wrong, log an error
     context.error("Hello, Errors!")
 
-    # The `ctx.req` object contains the request data
-    if context.req.method == "GET":
-        # Send a response with the res object helpers
-        # `ctx.res.send()` dispatches a string back to the client
-        return context.res.send(buf.getvalue())
+    # # The `ctx.req` object contains the request data
+    # if context.req.method == "GET":
+    #     # Send a response with the res object helpers
+    #     # `ctx.res.send()` dispatches a string back to the client
+    #     return context.res.send(buf.getvalue())
 
     # `ctx.res.json()` is a handy helper for sending JSON
     return context.res.json(
