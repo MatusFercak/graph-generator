8156ab350fd2ee230280912796d3dc63919620d9
Matúš Ferčák <74977647+MatusFercak@users.noreply.github.com>
2023-10-01 15:33:20 +0200

    patch

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ def main(context):
     if context.req.method == "GET":
         # Send a response with the res object helpers
         # `ctx.res.send()` dispatches a string back to the client
-        return context.res.send(buf.getbuffer())
+        return context.res.send(buf.getvalue())
 
     # `ctx.res.json()` is a handy helper for sending JSON
     return context.res.json(
