db87c1ff6bbc7836b3302a3a023d8e0bf8ca56d2
Matúš Ferčák <74977647+MatusFercak@users.noreply.github.com>
2023-10-02 17:26:05 +0200

    patch

--- src/main.py
+++ src/main.py
@@ -4,18 +4,21 @@ from io import BytesIO
 
 
 def main(context):
-    try:
-        check(context)
-        body = dict(context.req.body)
-        plt.plot(body["x"], body["y"])
-        plt.title(body["title"])
-        plt.xlabel(body["x_label"])
-        plt.ylabel(body["y_label"])
+    if context.req.method == "GET":
+        if context.req.path == "/":
+            try:
+                check(context)
+                body = dict(context.req.body)
+                plt.plot(body["x"], body["y"])
+                plt.title(body["title"])
+                plt.xlabel(body["x_label"])
+                plt.ylabel(body["y_label"])
 
-        buf = BytesIO()
-        plt.savefig(buf, format="png")
-        return context.res.send(buf.getvalue())
-
-    except Exception as error:
-        context.error(error.message)
-        return context.res.json({"ok": False, "error": error.message}, 400)
+                buf = BytesIO()
+                plt.savefig(buf, format="png")
+                return context.res.send(buf.getvalue())
+            except Exception as error:
+                context.error(error.message)
+                return context.res.json({"ok": False, "error": error.message}, 400)
+        if context.req.path == "/params":
+            context.log(context.req.parameters)
